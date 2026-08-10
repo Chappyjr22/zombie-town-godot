811b1df39fa114762000618b1588f1356d10bac3
Jacob <jacobchapman3@gmail.com>
2026-08-10 16:53:30 -0400

    Support wall weapon ammo pricing

--- scripts/interactables/interactable.gd
+++ scripts/interactables/interactable.gd
@@ -5,13 +5,15 @@ extends Area3D
 @export var item_id: StringName = &""
 @export var display_name := "Interact"
 @export var cost := 0
+@export var ammo_cost := 0
 @export var interaction_range := 3.2
 
-func configure(kind: StringName, id: StringName, label: String, price: int) -> void:
+func configure(kind: StringName, id: StringName, label: String, price: int, ammo_price: int = 0) -> void:
 	interaction_kind = kind
 	item_id = id
 	display_name = label
 	cost = price
+	ammo_cost = ammo_price
 	collision_layer = 4
 	collision_mask = 0
 	monitoring = false
