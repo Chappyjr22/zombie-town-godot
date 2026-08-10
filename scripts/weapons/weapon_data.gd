c3a20451ddfab44bf71c929f293db998a23dd222
Jacob <jacobchapman3@gmail.com>
2026-08-10 15:49:35 -0400

    Add reusable weapon data resource

---
+++ scripts/weapons/weapon_data.gd
@@ -0,0 +1,16 @@
+class_name WeaponData
+extends Resource
+
+@export var id: StringName = &"weapon"
+@export var display_name := "Weapon"
+@export var damage := 50.0
+@export var headshot_multiplier := 3.0
+@export var magazine_size := 8
+@export var reserve_ammo := 96
+@export var reload_time := 1.6
+@export var fire_interval := 0.14
+@export var range := 100.0
+@export var recoil_pitch := 1.25
+@export var recoil_yaw := 0.35
+@export var hip_fov := 75.0
+@export var ads_fov := 58.0
