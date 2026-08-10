4cdb47781c3e8f4c8823d2ee38ac5ab6ceb606b8
Jacob <jacobchapman3@gmail.com>
2026-08-10 16:50:26 -0400

    Expand weapon data for wall-buy gun behaviors

--- scripts/weapons/weapon_data.gd
+++ scripts/weapons/weapon_data.gd
@@ -3,14 +3,22 @@ extends Resource
 
 @export var id: StringName = &"weapon"
 @export var display_name := "Weapon"
+@export var weapon_class: StringName = &"pistol"
+@export var fire_mode: StringName = &"semi"
 @export var damage := 50.0
 @export var headshot_multiplier := 3.0
+@export var pellets := 1
 @export var magazine_size := 8
 @export var reserve_ammo := 96
 @export var reload_time := 1.6
 @export var fire_interval := 0.14
 @export var range := 100.0
+@export var spread := 0.02
+@export var ads_spread := 0.004
 @export var recoil_pitch := 1.25
 @export var recoil_yaw := 0.35
 @export var hip_fov := 75.0
 @export var ads_fov := 58.0
+@export var shell_reload := false
+@export var wall_cost := 0
+@export var ammo_cost := 250
